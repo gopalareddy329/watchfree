e2f86abe3ee30a25e045490502b33021933b6ca7
Hrithik2212 <hr6466@srmist.edu.in>
2024-04-04 20:26:47 +0530

    movielens1M data download

---
+++ mldev/data/download_movielens1M.py
@@ -0,0 +1,7 @@
+from urllib.request import urlretrieve
+import zipfile
+
+urlretrieve("http://files.grouplens.org/datasets/movielens/ml-100k.zip", "movielens.zip")
+zip_ref = zipfile.ZipFile('movielens.zip', "r")
+zip_ref.extractall()
+print("Done.")
